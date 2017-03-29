18e776c52eb429628cb290bbc6d4803f7b4195ce
xDefo <andre.di.fini@gmail.com>
2017-03-29 21:58:43 +0200

    Add files via upload

---
+++ Progetto matematica/Mate/Mate/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Mate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.creaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.risolviToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuovaEspressioneToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.semplificaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Input_textbox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.creaToolStripMenuItem,
+            this.risolviToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1157, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // creaToolStripMenuItem
+            // 
+            this.creaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nuovaEspressioneToolStripMenuItem});
+            this.creaToolStripMenuItem.Name = "creaToolStripMenuItem";
+            this.creaToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.creaToolStripMenuItem.Text = "Crea";
+            // 
+            // risolviToolStripMenuItem
+            // 
+            this.risolviToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.semplificaToolStripMenuItem});
+            this.risolviToolStripMenuItem.Name = "risolviToolStripMenuItem";
+            this.risolviToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.risolviToolStripMenuItem.Text = "Risolvi";
+            // 
+            // nuovaEspressioneToolStripMenuItem
+            // 
+            this.nuovaEspressioneToolStripMenuItem.Name = "nuovaEspressioneToolStripMenuItem";
+            this.nuovaEspressioneToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.nuovaEspressioneToolStripMenuItem.Text = "Nuova espressione";
+            this.nuovaEspressioneToolStripMenuItem.Click += new System.EventHandler(this.nuovaEspressioneToolStripMenuItem_Click);
+            // 
+            // semplificaToolStripMenuItem
+            // 
+            this.semplificaToolStripMenuItem.Name = "semplificaToolStripMenuItem";
+            this.semplificaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.semplificaToolStripMenuItem.Text = "Semplifica";
+            this.semplificaToolStripMenuItem.Click += new System.EventHandler(this.semplificaToolStripMenuItem_Click);
+            // 
+            // Input_textbox
+            // 
+            this.Input_textbox.Location = new System.Drawing.Point(12, 27);
+            this.Input_textbox.Name = "Input_textbox";
+            this.Input_textbox.Size = new System.Drawing.Size(1133, 20);
+            this.Input_textbox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 53);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1133, 540);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1157, 605);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Input_textbox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem creaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuovaEspressioneToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem risolviToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem semplificaToolStripMenuItem;
+        private System.Windows.Forms.TextBox Input_textbox;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Progetto matematica/Mate/Mate/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mate
+{
+    public partial class Form1 : Form
+    {
+        string espressione = "";
+        string sottoEspressione = "";
+        private string sotto_Espressione;
+
+        public void riscrivi_Espressione()//riscrive espressione
+            {
+
+
+        }
+        public void semplifica() //semplifica l'espressione data
+        {
+
+            string a = "";
+            int primoNumero = 0;
+            string b = "";
+            int secondoNumero = 0;
+            int i = 0;
+            int inizio_staccoFraNumeri = 0;
+            int fine_staccoFraNumeri = 0;
+            while (espressione.Substring(i, 1) == "1" || espressione.Substring(i, 1) == "2" || espressione.Substring(i, 1) == "3" || espressione.Substring(i, 1) == "4" || espressione.Substring(i, 1) == "5" || espressione.Substring(i, 1) == "6" || espressione.Substring(i, 1) == "7" || espressione.Substring(i, 1) == "8" || espressione.Substring(i, 1) == "9" || espressione.Substring(i, 1) == "0")
+            {
+                a += espressione.Substring(i, 1);
+                i += 1;
+            }
+            inizio_staccoFraNumeri = i;
+            while (!(espressione.Substring(i, 1) == "1" || espressione.Substring(i, 1) == "2" || espressione.Substring(i, 1) == "3" || espressione.Substring(i, 1) == "4" || espressione.Substring(i, 1) == "5" || espressione.Substring(i, 1) == "6" || espressione.Substring(i, 1) == "7" || espressione.Substring(i, 1) == "8" || espressione.Substring(i, 1) == "9" || espressione.Substring(i, 1) == "0"))
+            {
+                i += 1;
+            }
+            fine_staccoFraNumeri = i;
+            while (i < espressione.Length)
+            {
+                if ((espressione.Substring(i, 1) == "1" || espressione.Substring(i, 1) == "2" || espressione.Substring(i, 1) == "3" || espressione.Substring(i, 1) == "4" || espressione.Substring(i, 1) == "5" || espressione.Substring(i, 1) == "6" || espressione.Substring(i, 1) == "7" || espressione.Substring(i, 1) == "8" || espressione.Substring(i, 1) == "9" || espressione.Substring(i, 1) == "0"))
+                {
+                    b += espressione.Substring(i, 1);
+                    i += 1;
+                }
+            }
+            primoNumero = Convert.ToInt32(a);
+            secondoNumero = Convert.ToInt32(b);
+            if (fine_staccoFraNumeri - inizio_staccoFraNumeri == 1)
+            {
+                if (espressione.Substring(inizio_staccoFraNumeri, 1) == "*")
+                {
+                    sotto_Espressione = Convert.ToString(primoNumero * secondoNumero);
+                }
+                else if (espressione.Substring(inizio_staccoFraNumeri, 1) == "/")
+                {
+
+                    sotto_Espressione = Convert.ToString(primoNumero) + "/" + Convert.ToString(secondoNumero);
+                }
+                else if (espressione.Substring(inizio_staccoFraNumeri, 1) == "+")
+                {
+                    sotto_Espressione = Convert.ToString(primoNumero + secondoNumero);
+                }
+                else if (espressione.Substring(inizio_staccoFraNumeri, 1) == "-")
+                {
+                    sotto_Espressione = Convert.ToString(primoNumero - secondoNumero);
+                }
+            }
+        }
+
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nuovaEspressioneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            espressione = Input_textbox.Text;
+        }
+
+        private void semplificaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+                semplifica();
+            textBox1.Text = espressione; 
+        }
+    }
+}
